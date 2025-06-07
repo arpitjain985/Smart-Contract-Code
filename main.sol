7e9040d4a62bf03b979492c6963b297d69df5d72
arpitjain985 <arpitjainporsa@gmail.com>
2025-06-07 19:01:36 +0530

    Update main.sol

--- main.sol
+++ main.sol
@@ -1,2 +1,11 @@
-gt
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.20;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+
+contract MyCustomToken is ERC20 {
+    constructor(uint256 initialSupply) ERC20("MyCustomToken", "MCT") {
+        _mint(msg.sender, initialSupply * 10 ** decimals());
+    }
+}
 
